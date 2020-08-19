818a308e1d5ff6db77875a24d70316f7a36560d4
Eduard Moser <eduard.moser@gmx.de>
2020-08-19 22:17:23 +0200

    initial

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int calc(int val) {
+  return 2 * val + 17;
+}
+
+int main() {
+  int i = 42;
+  printf("hello xxx: calc(%d) = %d\n", i, calc(i));
+  if ( calc(42) != 101 ) return -1;
+  if ( calc(42) != 101 ) return -1;
+  return 0;
+}
+
